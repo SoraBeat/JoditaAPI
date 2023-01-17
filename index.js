88a7c9ea6a12283fd7b1e6021a5f35096cb29d97
Roa Mazzola Lautaro Elian <30334118+SoraBeat@users.noreply.github.com>
2023-01-17 14:22:41 -0300

    Events public

--- index.js
+++ index.js
@@ -2,14 +2,18 @@ import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import rateLimit from "express-rate-limit";
+
 import userRouter from "./src/routes/user.route.js";
 import premiumRouter from "./src/routes/premium.route.js";
 import reportRouter from "./src/routes/reports.route.js";
 import eventRouter from "./src/routes/events.route.js";
+
 import authRouterPublic from "./src/routes/auth.route.js";
 import userRouterPublic from "./src/routes/user.public.route.js";
 import followRouterPublic from "./src/routes/follow.public.route.js";
 import reportRouterPublic from "./src/routes/reports.public.route.js";
+import eventRouterPublic from "./src/routes/events.public.route.js";
+
 import "./src/database/connectMongo.js";
 import path from "path";
 
@@ -30,14 +34,17 @@ app.use(cors());
 app.use(limiter);
 app.use(express.json());
 app.use(cookieParser());
+
 app.use("/api/private", userRouter);
 app.use("/api/private", premiumRouter);
 app.use("/api/private", reportRouter);
 app.use("/api/private", eventRouter);
+
 app.use("/api/public", authRouterPublic);
 app.use("/api/public", userRouterPublic);
 app.use("/api/public", followRouterPublic);
 app.use("/api/public", reportRouterPublic);
+app.use("/api/public", eventRouterPublic);
 
 //MongoDB Conecction
 app.get("/", (req, res) => res.sendFile(path.join(__dirname, "./index.html")));
---
+++ src/routes/events.public.route.js
@@ -0,0 +1,117 @@
+import { Router } from "express";
+import { query, body } from "express-validator";
+import validationResultExpress from "../middlewares/validationResultExpress.js";
+import { requireToken } from "../middlewares/requireToken.js";
+import {
+  getEvent,
+  getEventsPublic,
+  postEventPublic,
+  putEventPublic,
+  deleteEventPublic,
+} from "../controllers/events.controller.js";
+
+const router = Router();
+//Get all events
+router.get(
+  "/events",
+  [
+    query("search")
+      .trim()
+      .isLength({ max: 20 })
+      .withMessage("The search query param is to loong max 20 characters"),
+    query("eventType")
+      .trim()
+      .isLength({ max: 15 })
+      .withMessage("The search query param is to loong max 15 characters"),
+    requireToken,
+  ],
+  validationResultExpress,
+  getEventsPublic
+);
+//Get event by id
+router.get(
+  "/event/:eventId",
+  [
+    query("eventId")
+      .trim()
+      .isLength({ isLength: 24 })
+      .withMessage("The eventId param is incorrect, have to be 24 characters"),
+    requireToken,
+  ],
+  validationResultExpress,
+  getEvent
+);
+
+//Create event
+router.post(
+  "/event",
+  [
+    body("title", "Minimum 5 characters").trim().isLength({ min: 5 }),
+    body("title", "Maximum 20 characters").trim().isLength({ max: 20 }),
+    body("description", "Minimum 10 characters").trim().isLength({ min: 10 }),
+    body("description", "Maximum 100 characters").trim().isLength({ max: 100 }),
+    body("place", "Minimum 1 character").trim().isLength({ min: 1 }),
+    body("place", "Maximum 100 characters").trim().isLength({ max: 100 }),
+    body("eventType", "Minimum 5 characters").trim().isLength({ min: 5 }),
+    body("eventType", "Maximum 15 characters").trim().isLength({ max: 15 }),
+    body("capacity", "Minumum 1 Maximum 99999 of capacity")
+      .trim()
+      .isNumeric()
+      .isLength({ min: 1, max: 5 }),
+    body("tags", "Maximum 3 tags").isArray({ max: 3 }),
+    body("datetime", "The format of date is wrong").trim().isDate(),
+    body("price", "Minimum $0 Maximum $99.999")
+      .trim()
+      .isNumeric()
+      .isLength({ min: 0, max: 5 }),
+    requireToken,
+  ],
+  validationResultExpress,
+  postEventPublic
+);
+//Edit event
+router.put(
+  "/event/:eventId",
+  [
+    query("eventId")
+      .trim()
+      .isLength({ isLength: 24 })
+      .withMessage("The eventId param is incorrect, have to be 24 characters"),
+    body("title", "Minimum 5 characters").trim().isLength({ min: 5 }),
+    body("title", "Maximum 20 characters").trim().isLength({ max: 20 }),
+    body("description", "Minimum 10 characters").trim().isLength({ min: 10 }),
+    body("description", "Maximum 100 characters").trim().isLength({ max: 100 }),
+    body("place", "Minimum 1 character").trim().isLength({ min: 1 }),
+    body("place", "Maximum 100 characters").trim().isLength({ max: 100 }),
+    body("eventType", "Minimum 5 characters").trim().isLength({ min: 5 }),
+    body("eventType", "Maximum 15 characters").trim().isLength({ max: 15 }),
+    body("capacity", "Minumum 1 Maximum 99999 of capacity")
+      .trim()
+      .isNumeric()
+      .isLength({ min: 1, max: 5 }),
+    body("tags", "Maximum 3 tags").isArray({ max: 3 }),
+    body("datetime", "The format of date is wrong").trim().isDate(),
+    body("price", "Minimum $0 Maximum $99.999")
+      .trim()
+      .isNumeric()
+      .isLength({ min: 0, max: 5 }),
+    requireToken,
+  ],
+  validationResultExpress,
+  putEventPublic
+);
+
+//Delete event by id
+router.delete(
+  "/event/:eventId",
+  [
+    query("eventId")
+      .trim()
+      .isLength({ isLength: 24 })
+      .withMessage("The eventId param is incorrect, have to be 24 characters"),
+    requireToken,
+  ],
+  validationResultExpress,
+  deleteEventPublic
+);
+export default router;
